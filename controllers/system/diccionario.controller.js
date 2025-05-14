2cffbcbae30a84f3b23738633528dd5886e2cfac
Petter Meave Meracado <pettertwom@gmail.com>
2025-05-14 13:56:10 -0400

    ajustes

--- controllers/system/diccionario.controller.js
+++ controllers/system/diccionario.controller.js
@@ -61,9 +61,9 @@ const getDatos = async (req, res) => {
   cc.usucre creador, to_char(cc.fecmod, 'dd-mm-yyyy') fecha_modificacion, 
   cc.usumod modificador, cc.estado
   FROM codificacion.cod_catalogo cc
-  join codificacion.cod_catologo_ls ccl on ccl.catalogo = cc.catalogo 
+  join codificacion.cod_catologo_ls ccl on upper(ccl.catalogo) = upper(cc.catalogo)
   WHERE lower(cc.cat_cuest) = lower('${cuest}') and ccl.id_catalogo = '${cat}' and cc.estado='ACTIVO' ORDER BY 1;`;
-    console.log('getDatos ===> ', query);
+    // console.log('getDatos ===> ', query);
 
     await con.query(query, (err, result) => {
       if (err) {
@@ -91,12 +91,12 @@ const getDatos = async (req, res) => {
   }
 };
 const getDatosCatalogo = async (req, res) => {
-  // console.log('req-params getDatosCatalogo', req.params);
+
 
   let _user = await userData(req, res);
   var query;
   if (_user) {
-    console.log(req.params);
+    // console.log(req.params);
 
     if (req.params.tipo == 'cat') {
       query = `
@@ -105,9 +105,10 @@ const getDatosCatalogo = async (req, res) => {
   cc.usucre creador, to_char(cc.fecmod, 'dd-mm-yyyy') fecha_modificacion, 
   cc.usumod modificador, cc.estado
   FROM codificacion.cod_catalogo cc
-  join codificacion.cod_catologo_ls ccl on ccl.catalogo = cc.catalogo 
+  join codificacion.cod_catologo_ls ccl on  upper(ccl.catalogo) = upper(cc.catalogo)
   WHERE cc.id_catalogo = ${req.params.cod} and cc.estado='ACTIVO' ORDER BY 1;
   `;
+
     } else {
       query = `
    select cc.id_clasificador, cc.clas_desc, cc.clasificador, cc.codigo, cc.descripcion, cc.estado
@@ -115,7 +116,7 @@ const getDatosCatalogo = async (req, res) => {
   where cc.id_clasificador = ${req.params.cod} and cc.estado='ACTIVO' ORDER BY 1;
   `;
     }
-    console.log('getDatosCatalogo ===> ', query);
+    // console.log('getDatosCatalogo ===> ', query);
 
     await con.query(query, (err, result) => {
       if (err) {
@@ -186,13 +187,15 @@ const registerCatalogo = async (req, res) => {
     console.log(req.body, 'body');
 
     const { codigo, descripcion, cuestionario, catalogo, categoria, dataCat, categoria_select } = req.body;
+
     let duplicados = `
         select count(1)cant
         from codificacion.cod_catalogo ca
         where trim(upper(descripcion)) ilike trim(upper('${descripcion}'))
       `;
     const d = await con.query(duplicados);
-
+    // console.log(parseInt(d.rows[0].cant), 'cantidad');
+    
 
     if (parseInt(d.rows[0].cant) > 0) {
       return res.json({
@@ -204,10 +207,12 @@ const registerCatalogo = async (req, res) => {
       var query
       if (categoria == 'cat') {
         query = `
-                  INSERT INTO "codificacion"."cod_catalogo" ("cat_cuest", "cat_desc", "catalogo", "codigo", "descripcion", "estado", "usucre", "feccre", "usumod", "fecmod", "descripcion_unida", "unico") 
-                  VALUES ('${categoria_select}', null, '${dataCat}', '${codigo}', trim('${descripcion}'), 'ACTIVO', '${_user.login
+                  INSERT INTO "codificacion"."cod_catalogo" ( "cat_cuest","cat_desc", "catalogo", "codigo", "descripcion", "estado", "usucre", "feccre", "usumod", "fecmod", "descripcion_unida", "unico") 
+                  VALUES ('${cuestionario}', '${categoria_select}', '${catalogo}', '${codigo}', trim('${descripcion}'), 'ACTIVO', '${_user.login
           }', current_timestamp, NULL, NULL, NULL, 0);
       `;
+      // console.log(query, 'query adicionar catalogo');
+      
       } else {
         query = `
                   INSERT INTO "codificacion"."cod_clasificador" ("clas_desc", "clasificador", "codigo", "descripcion", "estado", "usucre", "feccre") 
@@ -220,7 +225,7 @@ const registerCatalogo = async (req, res) => {
           return res.json({
             title: "Error",
             icon: "error",
-            message: "Error al guardar el registro"
+            message: err.message
           });
         }
 
@@ -243,16 +248,28 @@ const registerCatalogo = async (req, res) => {
 const editRegister = async (req, res) => {
   let _user = await userData(req, res);
   if (_user) {
-    const { codigo, descripcion, id_catalogo } = req.body;
-    console.log(req.body, "req.body");
-    let query = `
+    const { codigo, descripcion, id_catalogo, categoria } = req.body;
+
+    // console.log(req.body, "req.body");
+    var query; 
+    if(categoria == 'cat'){
+    query = `
         UPDATE "codificacion"."cod_catalogo" 
         SET "codigo" = '${codigo}', "descripcion" = '${descripcion}', "usumod" = '${_user.login}', "fecmod" = CURRENT_TIMESTAMP 
         WHERE "id_catalogo" = ${id_catalogo};
     `;
+    }else{
+      query = `
+        UPDATE "codificacion"."cod_clasificador" 
+        SET "codigo" = '${codigo}', "descripcion" = '${descripcion}', "usumod" = '${_user.login}', "fecmod" = CURRENT_TIMESTAMP 
+        WHERE "id_clasificador" = ${id_catalogo};
+    `;
+    }
+    // console.log(query, "query Edit ");
+    
     await con.query(query, (err, result) => {
       if (err) {
-        console.log(err);
+        // console.log(err);
         return res.json({
           title: "Error",
           icon: "error",
@@ -321,7 +338,7 @@ const getDatosClasificador = async (req, res) => {
   where upper(clasificador) = upper('${req.params.clas_select}')
   and estado = 'ACTIVO'
   order by codigo`
-  console.log(query);
+  // console.log(query);
   await con.query(query, (err, result) => {
     if (err) {
       return res.json({
@@ -370,7 +387,7 @@ const getDatosCorrector = async (req, res) => {
     var result = await con.query(
       `select erradas, corregidas from codificacion.cod_err_corr coc where id_err_corr = ${req.params.ids} order by 1`
     );
-    console.log(result.rows);
+    // console.log(result.rows);
 
     return res.status(200).json({
       success: true,
